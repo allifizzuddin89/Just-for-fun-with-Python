f83aecf82769d26f539658a8a8cdddc0533ab597
allifizzuddin89 <allifizzuddin@gmail.com>
2020-07-21 14:22:49 +0800

    update 0.01

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,8 @@
+'''
+CREATED MAINLY FOR REVISING PYTHON SKILLS
+BY ALLIF IZZUDDIN BIN ABDULLAH
+'''
+
 #INITIAL VALUE OF BOARD
 board = {'1':' ', '2':' ', '3':' ', 
             '4':' ', '5':' ', '6':' ', 
@@ -16,7 +21,7 @@ def intro():
     print('+++++')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
-    print("\nRefer the position as follows: \n")
+    print("\nRefer the position of marker as follows: \n")
 
     print('7' + '|' + '8' + '|' + '9')
     print('+++++')
@@ -26,19 +31,20 @@ def intro():
 
 #PLAYER BOARD ASSIGNMENT 
 def print_board(player,post):
+    board_print = board
     if player == 1:
         post = str(post)
-        board[post] = 'X'
+        board_print[post] = 'X'
     if player == 2:
         post = str(post)
-        board[post] = 'O'
-    
+        board_print[post] = 'O'
+    print(f'Board print is {board_print}')
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('+++++')
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('+++++')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    return board
+    return board_print
 
 #ASSIGN THE PLAYER (1 OR 2)
 def marker(player=0, marker='X'):
@@ -62,7 +68,7 @@ def marker(player=0, marker='X'):
 #CHECK THE TIC RANGE
 def julat(tic=0):
     while True:
-        tic = input('Please mark your board :')
+        tic = input('\nPlease mark your board :')
         if tic.isdigit() == True:
             tic1 = int(tic)
             #print(tic1)
@@ -78,92 +84,100 @@ def julat(tic=0):
 #REMOVE MARK THAT HAS BEEN TAKEN
 def position(tic):
     tic = str(tic)
-    print(tic)
-    display.pop(tic)
-    print(display)
-    return display, tic
+    #print(tic)
+    display_tic = display
+    display_tic.pop(tic)
+    x =len(display_tic)
+    #print('\n',display,'\n')
+    #print(f'Length of the dict is {x}')
+    return x,display_tic
 
 #CHECK IF THE MARK IS EXIST
 def checkMark(board1,tic):
-    checkok = False
     print(board1)
     #tic = str(tic)
-    print('The mark is {}'.format(board1[tic]))
-    while checkok == False:
-        if tic in board1.keys():
-            checkok = True
-            break
-        elif tic not in board1.keys():
-            print("The mark already taken")
-            break
+    #print('The mark is {}'.format(board1[tic]))
+    
+    if tic in board1.keys() == True:
+        pass
+    elif tic in board1.keys() == False:
+        print("The mark already taken")
+        
 
 
 #CHECK WHO IS THE WINNER
-def winner(board,player):
-    if (board['7'] == board['4'] and board['7'] == board['1']):
-        if board['7'] != ' ' or board['4'] != ' ' or board['1'] != ' ' :      
+def winner(board1,player):
+    if board1['7'] == board1['4']:
+        if board1['7'] == board1['1']:
+            if board1['7'] != ' ' or board1['4'] != ' ' or board1['1'] != ' ' :      
+                print('Player {} WIN\n'.format(player))
+                print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
+                print('+++++')
+                print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
+                print('+++++')
+                print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+                return False
+    elif board1['7'] == board1['5'] and board1['7'] == board1['3']:
+        if board1['7'] != ' ' or board1['5'] != ' ' or board1['3'] != ' ' :
             print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
             print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
             print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
-
-    elif board['7'] == board['5'] and board['7'] == board['3']:
-        if board['7'] != ' ' or board['5'] != ' ' or board['3'] != ' ' :
+            print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+            return False
+    elif (board1['7'] == board1['8'] and board1['7'] == board1['9']) :
+        if board1['7'] != ' ' or board1['8'] != ' ' or board1['9'] != ' ' :
             print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
             print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
             print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    elif (board['7'] == board['8'] and board['7'] == board['9']) :
-        if board['7'] != ' ' or board['8'] != ' ' or board['9'] != ' ' :
+            print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+            return False
+    elif (board1['8'] == board1['5'] and board1['8'] == board1['2']) :
+        if board1['8'] != ' ' or board1['5'] != ' ' or board1['2'] != ' ' :
             print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
             print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
             print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    elif (board['8'] == board['5'] and board['8'] == board['2']) :
-        if board['8'] != ' ' or board['5'] != ' ' or board['2'] != ' ' :
+            print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+            return False
+    elif (board1['9'] == board1['5'] and board1['9'] == board1['1']) :
+        if board1['9'] != ' ' or board1['5'] != ' ' or board1['1'] != ' ' :
             print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
             print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
             print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    elif (board['9'] == board['5'] and board['9'] == board['1']) :
-        if board['9'] != ' ' or board['5'] != ' ' or board['1'] != ' ' :
+            print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+            return False
+    elif (board1['9'] == board1['6'] and board1['9'] == board1['3']) :
+        if board1['9'] != ' ' or board1['6'] != ' ' or board1['3'] != ' ' :
             print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
             print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
             print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    elif (board['9'] == board['6'] and board['9'] == board['3']) :
-        if board['9'] != ' ' or board['6'] != ' ' or board['3'] != ' ' :
+            print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+            return False
+    elif (board1['1'] == board1['2'] and board1['1'] == board1['3']) :
+        if board1['1'] != ' ' or board1['2'] != ' ' or board1['3'] != ' ' :
             print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
             print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
             print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    elif (board['1'] == board['2'] and board['1'] == board['3']) :
-        if board['1'] != ' ' or board['2'] != ' ' or board['3'] != ' ' :
-            print('Player {} WIN\n'.format(player))
-            print(board['7'] + '|' + board['8'] + '|' + board['9'])
-            print('+++++')
-            print(board['4'] + '|' + board['5'] + '|' + board['6'])
-            print('+++++')
-            print(board['1'] + '|' + board['2'] + '|' + board['3'])
+            print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+            return False
     else :
-        print(board['7'] + '|' + board['8'] + '|' + board['9'])
+        print(board1['7'] + '|' + board1['8'] + '|' + board1['9'])
         print('+++++')
-        print(board['4'] + '|' + board['5'] + '|' + board['6'])
+        print(board1['4'] + '|' + board1['5'] + '|' + board1['6'])
         print('+++++')
-        print(board['1'] + '|' + board['2'] + '|' + board['3'])
-        return True, player
+        print(board1['1'] + '|' + board1['2'] + '|' + board1['3'])
+        return True
 
 def sambung():
     while True:
@@ -238,16 +252,34 @@ while tictactoe == True:
 #problem
 #checkMark(update_board,post)
 
-update_board ={'7':'X', '8':'X', '9':' ', 
-            '4':'X', '5':' ', '6':' ', 
-            '1':'O', '2':' ', '3':'X'}
+# update_board ={'7':'X', '8':'X', '9':' ', 
+#             '4':'X', '5':' ', '6':' ', 
+#             '1':'O', '2':' ', '3':'X'}
 
-#print(update_board)
+# #print(update_board)
 
-#winner(update_board,1)
+
+# replay = winner(update_board,1)
+# print(replay)
 
 #print(type(update_board))
 
 #position(1)
 
-checkMark(update_board,2)
+# checkMark(update_board,2)
+
+intro()
+pemain, tanda = marker()
+posisi = julat()
+checkMark(display,posisi)
+board_display = print_board(pemain,posisi)
+count_marker,display_tic1 = position(posisi) #fix it! globally delete keys
+print(f'Length of the dict left is {count_marker}')
+print(f'The board iteration as follows : {display_tic1}')
+print(f'\nThe board display is {board_display}')
+print(f'\nPemain {pemain}')
+
+winner(board_display,pemain) # fix it! not working!
+#https://stackoverflow.com/questions/25905300/modifying-global-dictionary-in-python-within-a-function
+#https://www.dataquest.io/blog/tutorial-functions-modify-lists-dictionaries-python/
+#
